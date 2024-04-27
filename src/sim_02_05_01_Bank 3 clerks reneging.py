eefca0278e1bdb43ac3de4c378d6060f667a909e
FrankLef <flefebvre01@hotmail.com>
2024-04-27 15:11:50 -0400

    feat: sim 02 05

--- src/sim_02_05_01_Bank 3 clerks reneging.py
+++ src/sim_02_05_01_Bank 3 clerks reneging.py
@@ -10,17 +10,34 @@ class CustomerGenerator(sim.Component):
 
 class Customer(sim.Component):
     def process(self):
-        self.request(clerks)
-        self.hold(30)
-        self.release()  # not really required
+        if len(waitingline) >= 5:
+            env.number_balked += 1
+            env.print_trace("", "", "balked")
+            print(env.now(), "balked", self.name())
+            self.cancel()
+        self.enter(waitingline)
+        for clerk in clerks:
+            if clerk.ispassive():
+                clerk.activate()
+                break  # activate only 1 clerk
+        self.hold(50)  # if not served within this time, renege
+        if self in waitingline:
+            self.leave(waitingline)
+            env.number_reneged += 1
+            env.print_trace("", "", "reneged")
+        else:
+            self.passivate()  # wait for service to be completed
 
 
 class Clerk(sim.Component):
     def process(self):
         while True:
-            customer = self.from_store(waitingroom)
+            while len(waitingline) == 0:
+                self.passivate()
+            self.customer = waitingline.pop()
+            self.customer.activate()  # get the cutomer out of it's hold(50)
             self.hold(30)
-            customer.activate()
+            self.customer.activate()  # signal the customer it's done
 
 
 # def main():
@@ -39,10 +56,12 @@ if __name__ == "__main__":
     # main()
     env = sim.Environment(trace=False)
     CustomerGenerator()
+    env.number_reneged = 0
+    env.number_balked = 0
+    clerks = [Clerk() for _ in range(3)]
     waitingline = sim.Queue("waitingline")
-    waitingroom = sim.Store("waitingroom")
-    clerks = sim.Queue(name="clerks", fill=[Clerk() for _ in range(3)])
-    clerks = sim.Resource("clerks", capacity=3)
-    env.run(till=50000)
-    clerks.print_statistics()
-    clerks.print_info()
+    env.run(till=300000)
+    waitingline.length.print_statistics()
+    waitingline.length_of_stay.print_statistics()
+    print("number reneged", env.number_reneged)
+    print("number balked", env.number_balked)
