b831a4de0f574a553820ea7603f5727a66a1187b
FrankLef <flefebvre01@hotmail.com>
2024-04-28 07:56:12 -0400

    feat: sim 02 08

---
+++ src/sim_02_08_01_Bank 3 clerks states.py
@@ -0,0 +1,36 @@
+import salabim as sim  # type: ignore
+
+
+class CustomerGenerator(sim.Component):
+    def process(self):
+        while True:
+            Customer()
+            self.hold(sim.Uniform(5, 15).sample())
+
+
+class Customer(sim.Component):
+    def process(self):
+        self.enter(waitingline)
+        worktodo.trigger(max=1)
+        self.passivate()
+
+
+class Clerk(sim.Component):
+    def process(self):
+        while True:
+            if len(waitingline) == 0:
+                self.wait((worktodo, True, 1))
+            self.customer = waitingline.pop()
+            self.hold(30)
+            self.customer.activate()
+
+
+if __name__ == "__main__":
+    # main()
+    env = sim.Environment(trace=False)
+    CustomerGenerator()
+    waitingline = sim.Queue("waitingline")
+    worktodo = sim.State("worktodo")
+    env.run(till=50000)
+    waitingline.print_histograms()
+    worktodo.print_histograms()
